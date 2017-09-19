fe8a192ba0672e9c81514437a0d8bfd1fc15b6e2
Ashwin Gopalakrishnan <ashwin@dianomic.com>
2017-09-18 20:23:30 -0700

    FOGL-507 configured setup.py

--- src/python/setup.py
+++ src/python/setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    requires = f.read().splitlines()
+
 setup(
     name='FogLAMP',
     version='0.1',
@@ -8,11 +11,23 @@ setup(
     author='OSIsoft, LLC',
     author_email='info@dianomic.com',
     license='Apache 2.0',
-    packages=['foglamp'],
+    install_requires=requires,
+    packages=[
+    'foglamp.translators',
+    'foglamp.device',
+    'foglamp.data_purge',
+    'foglamp.admin_api',
+    'foglamp',
+    'foglamp.core.api',
+    'foglamp.core',
+    'foglamp.core.service_registry',
+    ],
     entry_points={
         'console_scripts': [
             'foglamp = foglamp.core.server_daemon:main'
         ],
     },
     zip_safe=False
+
 )
+
