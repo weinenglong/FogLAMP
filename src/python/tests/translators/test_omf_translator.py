3afb453c2b4bcb3bf80d4f027f17fbe2c2fa280e
Ori Shadmon <oshadmon@gmail.com>
2017-10-23 21:28:46 +0100

    Testing of omf_translator for plugin_send and plugin_retrieve_plugin_info

--- src/python/tests/translators/test_omf_translator.py
+++ src/python/tests/translators/test_omf_translator.py
@@ -1,78 +1,99 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# FOGLAMP_BEGIN
+# See: http://foglamp.readthedocs.io/
+# FOGLAMP_END
+
+"""The following code tests the omf_translator connector""" 
+
+import aiopg.sa
+import asyncio
 import os
 import pytest 
+from psycopg2 import DataError
 from foglamp.translators import omf_translator
 
-#@pytest.allure.feature("unit")
-#@pytest.allure.story("configuration manager")
-class TestOMFTranslator:
+__author__ = "Ori Shadmon"
+__copyright__ = "Copyright (c) 2017 OSIsoft, LLC"
+__license__ = "Apache 2.0"
+__version__ = "${VERSION}"
+
+_CONNECTION_STRING="dbname='foglamp'"
+
+# Tests for plugin_retrieve_plugin_info
+def test_plugin_retrieve_plugin_info():
     """
-    The following class tests the following functions from foglamp.translators 
-      def plugin_retrieve_info(stream_id):
-      def plugin_init():
-      def plugin_send(raw_data, stream_id):
-      def plugin_shutdown():
+    Test that omf_translator.plugin_retrieve_info return valid values
+    :assert:
+        Assert omf_translator.plugin_retrieve_info is as expected
     """ 
-    def setup_method(self):
-        raw_output = os.popen("foglamp start")
-        output = raw_output.read()
-        raw_output.close()
-
-    def teardown_method(self):
-        raw_output = os.popen("foglamp stop")
-        output = raw_output.read()
-        raw_output.close()
+    expect = {
+      'type': 'translator', 
+      'config': {
+        'OMFRetrySleepTime': 1, 
+        'producerToken': 'omf_translator_0001', 
+        'OMFMaxRetry': 5, 
+        'OMFHttpTimeout': 30, 
+        'URL': 'http://WIN-4M7ODKB0RH2:8118/ingress/messages', 
+        'StaticData': {
+          'Location': 'Palo Alto', 
+          'Company': 'Dianomic'
+        }
+      }, 
+      'interface': '1.0', 
+      'version': '1.0.0', 
+      'name': 'OMF Translator'
+    }
+    actual = omf_translator.plugin_retrieve_info(1)
     
-    def test_plugin_init(self): 
-        omf_translator.plugin_init()
+    for key in expect:
+        if key == 'config':
+            for key2 in expect[key]:
+                if key2 == 'StaticData': 
+                    for key3 in expect[key][key2]: 
+                        assert expect[key][key2][key3] == actual[key][key2][key3]
+                else: 
+                    assert expect[key][key2] == actual[key][key2]
+        else: 
+            assert expect[key] == actual[key]
 
-    def test_plugin_retreive_info_error(self):
-        """
-        Test behavior when omf_translator.plugin_retrieve_info() doesn't contain anythng
-        :assert:
-            Error gets returned
-        """
+def test_plugin_retrieve_plugin_info_errors():
+    """ 
+    For plugin_retrieve_plugin_info check errors 
+    :assert: 
+        1. TypeError when no value set for stream_id
+        2. psycopg2.DataError when stream_id > len 10
+    """ 
+    with pytest.raises(TypeError) as error_exec:
+        acutal = omf_translator.plugin_retrieve_info()
+    assert "TypeError: plugin_retrieve_info() missing 1 required positional argument: 'stream_id'" in str(error_exec)
+    with pytest.raises(DataError) as error_exec:
+        actual = omf_translator.plugin_retrieve_info("stream_id_01")
+    assert "psycopg2.DataError: value too long for type character(10)" in str(error_exec)
+
+# Tests for plugin_send
+def test_plugin_send():
+    """
+    When doing plugin_send and setting some values to `raw_data` an error 
+    gets returned "unexpected keyword argument". I'm not entirely sure 
+    whether whether this an issue with how I call the code 
+    """
+    values = [{}, [], (), "aaa", 'aaa']
+    output=''
+    for value in values: 
         with pytest.raises(TypeError) as error_exec:
-            omf_translator.plugin_retrieve_info()
-        assert "plugin_retrieve_info() missing 1 required positional argument: 'stream_id'" in str(error_exec) 
-    
-    def test_plugin_retrieve_info(self):
-        """
-        Since as of now the `stream_id` does not affect 
-        the output, the test compares the plugin_retrive_info
-        with `stream_id` of 0 and 1
-        :assert:
-            plugin_retrive_info returns consistent info
-        """ 
-        output0 = omf_translator.plugin_retrieve_info(0)
-        output1 = omf_translator.plugin_retrieve_info(1)
-        for key in output0:
-            if key == 'config':
-                for key2 in output0[key]:
-                    if key2 == 'StaticData':
-                        for key3 in output0[key][key2]:
-                            assert output0[key][key2][key3] == output1[key][key2][key3]
-                    else:
-                        assert output0[key][key2] == output1[key][key2] 
-            else:
-                assert output0[key] == output1[key] 
+            output = omf_translator.plugin_send(raw_data=value, stream_id=1)
+        print(value, error_exec)
+        print(output)
 
-    def test_plugin_init_error(self): 
-        with pytest.raises(AttributeError) as error_exec: 
-            omf_translator.plugin_init()
-        assert "'str' object has no attribute 'debug'" in str(error_exec)
+def test_plugin_send_error():
+    """
+    Test error gets returned when raw_data and stream_id are missing
+    :assert:
+        Assert TypeError returned
+    """
+    with pytest.raises(TypeError) as error_exec: 
+        actual = omf_translator.plugin_send()
+    assert "TypeError: plugin_send() missing 2 required positional arguments: 'raw_data' and 'stream_id'" in str(error_exec)
 
-    def test_plugin_send_empty_error(self):
-        """
-        Test error gets returned when 1 or more variables is missing
-        :assert:
-            TypeError when both raw_data and stream_id empty
-        """ 
-        with pytest.raises(TypeError) as error_exec:
-            omf_translator.plugin_send()
-        assert "TypeError: plugin_send() missing 2 required positional arguments: 'raw_data' and 'stream_id'" in str(error_exec)
-    
-    def test_plugin_send_invalid_value(self):
-        omf_translator.plugin_send(raw_data='', stream_id=1)
-        
-   
-        
